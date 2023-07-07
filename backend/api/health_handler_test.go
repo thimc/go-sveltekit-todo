92bfaff59d04051511ed084d35cca3409e64a747
Thim Cederlund <thim@cederlund.de>
2023-07-07 17:25:34 +0200

    add backend/api/health_handler_test.go

---
+++ backend/api/health_handler_test.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func TestHealthCheck(t *testing.T) {
+	store := setup(t)
+	defer store.Close()
+
+	app := fiber.New()
+	app.Get("/health", HealthCheck)
+
+	req := httptest.NewRequest("GET", "/health", nil)
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("expected http code '%d', got '%d'", http.StatusOK, resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(body) != "OK" {
+		t.Fatalf("expected body '%s', got '%s'", "OK", string(body))
+	}
+}
