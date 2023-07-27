035e4cc27a920062701ce3ec5f46e5b9fde69c09
Thim Cederlund <thim@cederlund.de>
2023-07-27 20:17:00 +0200

    cleanup

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -3,10 +3,7 @@ import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ resolve, event }) => {
 	event.locals.user = await authenticateUser(event);
-	// console.log('Hook handle - Auth:', event.locals.user?.token);
-	// console.log('Cookie:', event.cookies.get('jwt'));
 
-	/* Protect everything except login */
 	if (event.url.pathname == '/') {
 		if (!event.locals.user) {
 			throw redirect(303, '/login');
